c268380e77aea14bff892f5ffe8fb96088822466
Oliver.W <ole3021@gmail.com>
2019-04-03 17:28:15 +0800

    fix: jest prettier config

--- src/recaptcha-server.ts
+++ src/recaptcha-server.ts
@@ -1,37 +1,37 @@
-'use strict'
+'use strict';
 
-import axios, { AxiosInstance } from 'axios'
-import { PardError } from './utils'
+import axios, { AxiosInstance } from 'axios';
+import { PardError } from './utils';
 
 interface VerifyV3Response {
-  isPassed: boolean
-  score: number
-  hostName: string
-  action: string
-  checkedAt: Date
+  isPassed: boolean;
+  score: number;
+  hostName: string;
+  action: string;
+  checkedAt: Date;
 }
 
 export default class Recaptcha {
-  private recapAxios: AxiosInstance
+  private recapAxios: AxiosInstance;
   constructor(private secret: string, private timeout = 3000) {
     this.recapAxios = axios.create({
       baseURL: 'https://www.recaptcha.net',
       timeout
-    })
+    });
   }
 
   async verifyV3Async(token: string, minScore = 0.6): Promise<VerifyV3Response> {
     try {
       interface AxiosResponse {
-        data: AxiosRecaptchaResult
+        data: AxiosRecaptchaResult;
       }
       interface AxiosRecaptchaResult {
-        success: boolean
-        score: number
-        action: string
-        challenge_ts: string
-        hostname: string
-        'error-codes'?: string[]
+        success: boolean;
+        score: number;
+        action: string;
+        challenge_ts: string;
+        hostname: string;
+        'error-codes'?: string[];
       }
 
       const { data: verifyResult }: AxiosResponse = await this.recapAxios.post(
@@ -43,7 +43,7 @@ export default class Recaptcha {
             response: token
           }
         }
-      )
+      );
 
       const {
         success,
@@ -52,12 +52,12 @@ export default class Recaptcha {
         hostname,
         challenge_ts,
         'error-codes': errorCodes
-      } = verifyResult
+      } = verifyResult;
 
       if (!success) {
         throw new PardError(`[recaptcha-server] Failed to verify recaptcha: ${errorCodes}`, {
           type: 'RECAPTCHA_VERIFY_ERROR'
-        })
+        });
       }
 
       return {
@@ -66,10 +66,10 @@ export default class Recaptcha {
         hostName: hostname,
         action,
         checkedAt: new Date()
-      }
+      };
     } catch (error) {
-      error.type = error.type || 'RECAPTCHA_UNEXPECTED_ERROR'
-      throw error
+      error.type = error.type || 'RECAPTCHA_UNEXPECTED_ERROR';
+      throw error;
     }
   }
 }
--- test/recaptcha-server.test.ts
+++ test/recaptcha-server.test.ts
@@ -1,13 +1,13 @@
-'use strict'
+'use strict';
 
-import Recaptcha from '../src/recaptcha-server'
-import * as nock from 'nock'
+import Recaptcha from '../src/recaptcha-server';
+import * as nock from 'nock';
 
 describe('Recaptcha', () => {
-  const recInstance = new Recaptcha('testSecret')
+  const recInstance = new Recaptcha('testSecret');
   it('should can init instance with secret', () => {
-    expect(recInstance).toBeInstanceOf(Recaptcha)
-  })
+    expect(recInstance).toBeInstanceOf(Recaptcha);
+  });
 
   it('should can throw error when failed', async () => {
     nock('https://www.recaptcha.net')
@@ -23,15 +23,15 @@ describe('Recaptcha', () => {
         challenge_ts: '2019-04-03T14:00:00ZZ',
         hostname: 'dozto.com',
         'error-codes': ['invalid-secret']
-      })
+      });
 
     try {
-      const recFakeInstance = new Recaptcha('fakeSecret')
-      await recFakeInstance.verifyV3Async('testToken')
+      const recFakeInstance = new Recaptcha('fakeSecret');
+      await recFakeInstance.verifyV3Async('testToken');
     } catch (error) {
-      expect(error.type).toEqual('RECAPTCHA_VERIFY_ERROR')
+      expect(error.type).toEqual('RECAPTCHA_VERIFY_ERROR');
     }
-  })
+  });
 
   it('should can verify with correct data', async () => {
     nock('https://www.recaptcha.net')
@@ -46,12 +46,12 @@ describe('Recaptcha', () => {
         action: 'contactUs',
         challenge_ts: '2019-04-03T14:00:00ZZ',
         hostname: 'dozto.com'
-      })
+      });
 
-    const v3Result = await recInstance.verifyV3Async('testToken1')
+    const v3Result = await recInstance.verifyV3Async('testToken1');
 
-    expect(v3Result.isPassed).toBeTruthy()
-  })
+    expect(v3Result.isPassed).toBeTruthy();
+  });
 
   it('should can verify with correct data and custom score', async () => {
     nock('https://www.recaptcha.net')
@@ -66,9 +66,9 @@ describe('Recaptcha', () => {
         action: 'contactUs',
         challenge_ts: '2019-04-03T14:00:00ZZ',
         hostname: 'dozto.com'
-      })
+      });
 
-    const v3Result = await recInstance.verifyV3Async('testToken2', 0.7)
-    expect(v3Result.isPassed).toBeFalsy()
-  })
-})
+    const v3Result = await recInstance.verifyV3Async('testToken2', 0.7);
+    expect(v3Result.isPassed).toBeFalsy();
+  });
+});
