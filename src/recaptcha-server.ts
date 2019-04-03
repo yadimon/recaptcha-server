4f6a0e00d12cb3b14b866a9531293a2f29306d9a
Oliver.W <ole3021@gmail.com>
2019-04-03 17:11:37 +0800

    feat: complete recaptcha feature

--- src/recaptcha-server.ts
+++ src/recaptcha-server.ts
@@ -1,4 +1,15 @@
+'use strict'
+
 import axios, { AxiosInstance } from 'axios'
+import { PardError } from './utils'
+
+interface VerifyV3Response {
+  isPassed: boolean
+  score: number
+  hostName: string
+  action: string
+  checkedAt: Date
+}
 
 export default class Recaptcha {
   private recapAxios: AxiosInstance
@@ -9,14 +20,56 @@ export default class Recaptcha {
     })
   }
 
-  async verifyV3Async(token: string, minScore = 0.6): Promise<boolean> {
-    const { data: verifyResult } = await this.recapAxios.post('recaptcha/api/siteverify', null, {
-      params: {
-        secret: this.secret,
-        response: token
+  async verifyV3Async(token: string, minScore = 0.6): Promise<VerifyV3Response> {
+    try {
+      interface AxiosResponse {
+        data: AxiosRecaptchaResult
       }
-    })
+      interface AxiosRecaptchaResult {
+        success: boolean
+        score: number
+        action: string
+        challenge_ts: string
+        hostname: string
+        'error-codes'?: string[]
+      }
 
-    return (verifyResult.score && verifyResult.score >= minScore) || false
+      const { data: verifyResult }: AxiosResponse = await this.recapAxios.post(
+        'recaptcha/api/siteverify',
+        null,
+        {
+          params: {
+            secret: this.secret,
+            response: token
+          }
+        }
+      )
+
+      const {
+        success,
+        score,
+        action,
+        hostname,
+        challenge_ts,
+        'error-codes': errorCodes
+      } = verifyResult
+
+      if (!success) {
+        throw new PardError(`[recaptcha-server] Failed to verify recaptcha: ${errorCodes}`, {
+          type: 'RECAPTCHA_VERIFY_ERROR'
+        })
+      }
+
+      return {
+        isPassed: success && score > minScore,
+        score,
+        hostName: hostname,
+        action,
+        checkedAt: new Date()
+      }
+    } catch (error) {
+      error.type = error.type || 'RECAPTCHA_UNEXPECTED_ERROR'
+      throw error
+    }
   }
 }
